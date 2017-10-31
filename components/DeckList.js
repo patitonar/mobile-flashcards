593bfa936cacd6907ae8c713f5f817d93c4af8c8
Gerardo Nardelli <patitonardelli@gmail.com>
2017-10-31 20:39:43 -0300

    feat: list decks

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { initializeState, getDecks } from '../utils/api';
 
 class DeckList extends Component {
+  state = { decks: {} };
+  componentDidMount() {
+    initializeState().then(getDecks().then(decks => this.setState({ decks })));
+  }
+
   render() {
+    const { decks } = this.state;
+    const array = Object.keys(decks).map(key => decks[key]);
     return (
-      <View
-        style={{
-          flex: 1
-        }}
-      >
-        <Text>Deck List!</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <FlatList
+          data={array}
+          keyExtractor={(item, index) => item.title}
+          renderItem={({ item }) => (
+            <Text key={item.title}>
+              {item.title} - Num: {item.questions.length}
+            </Text>
+          )}
+        />
       </View>
     );
   }
