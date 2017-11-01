1645e77cce9148a2ce4a38cc72ca7f61cc520284
Gerardo Nardelli <patitonardelli@gmail.com>
2017-11-01 19:50:05 -0300

    feat: add style to DeckList

--- components/DeckList.js
+++ components/DeckList.js
@@ -16,20 +16,48 @@ class DeckList extends Component {
     const { decks } = this.props;
     const array = Object.keys(decks).map(key => decks[key]);
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1 }}>
         <FlatList
           data={array}
           keyExtractor={(item, index) => item.title}
           renderItem={({ item }) => (
-            <Text key={item.title}>
-              {item.title} - Num: {item.questions.length}
-            </Text>
+            <View key={item.title} style={styles.item}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.subtitle}>
+                {item.questions ? item.questions.length : 0} cards
+              </Text>
+            </View>
           )}
         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    flex: 1,
+    backgroundColor: 'white',
+    borderRadius: 2,
+    padding: 24,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowRadius: 3,
+
+    shadowColor: 'rgba(0, 0, 0, 0.2)',
+    shadowOffset: {
+      width: 3,
+      height: 3
+    }
+  },
+  title: {
+    fontSize: 24
+  },
+  subtitle: { fontSize: 14 }
+});
 
 function mapStateToProps(decks) {
   return {
